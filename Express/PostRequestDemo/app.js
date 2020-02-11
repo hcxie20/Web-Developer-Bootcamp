868c150385c01ba7a611bf92c1c0cd364c52c03c
Ubuntu <ubuntu@ip-172-31-4-2.us-west-1.compute.internal>
2020-02-11 00:28:05 +0000

    done express

--- Express/PostRequestDemo/app.js
+++ Express/PostRequestDemo/app.js
@@ -1,14 +1,24 @@
 var express = require("express")
 var app = express()
 
-app.set("view engine", "ejs")
+var bodyParser = require("body-parser")
 
+var friends = ["1", "2", "3"]
+
+app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({extended: true}))
 app.get("/", function(req, res){
     res.render("home")
 })
 
 app.get("/friends", function(req, res){
-    res.render("friends")
+    res.render("friends", {friends:friends})
+})
+
+app.post("/addfriend", function(req, res){
+    console.log(req.body)
+    friends.push(req.body.newfriend)
+    res.redirect("friends")
 })
 
 app.listen(3000, function(){
