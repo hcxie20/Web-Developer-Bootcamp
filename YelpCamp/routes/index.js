0cdc295d5b8592bfc9168f4fdff77044d76e36f2
hcxie20 <hcxie20@126.com>
2020-02-18 13:29:53 -0800

    done

--- YelpCamp/routes/index.js
+++ YelpCamp/routes/index.js
@@ -38,12 +38,6 @@ router.get("/logout", function(req, res){
     res.redirect("/campgrounds")
 })
 
-function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()){
-        return next()
-    }else{
-        res.redirect("/login")
-    }
-}
+
 
 module.exports = router
